27c5a316cc531c9e9a23a0634cf16bf1a1ac29d3
latavin243 <arvin.qguo@outlook.com>
2023-11-20 14:08:33 +0800

    add strcase unittest

---
+++ strcase/strcase_test.go
@@ -0,0 +1,36 @@
+package strcase_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/latavin243/goutils/strcase"
+)
+
+func TestCamelToSnake(t *testing.T) {
+	testCases := []struct {
+		input, expected string
+	}{
+		{"UserTotalCnt", "user_total_cnt"},
+	}
+	for _, tc := range testCases {
+		got := strcase.CamelToSnake(tc.input)
+		fmt.Printf("got: %s\n", got)
+		assert.Equal(t, tc.expected, got)
+	}
+}
+
+func TestSnakeToTitle(t *testing.T) {
+	testCases := []struct {
+		input, expected string
+	}{
+		{"user_total_cnt", "UserTotalCnt"},
+	}
+	for _, tc := range testCases {
+		got := strcase.SnakeToTitle(tc.input)
+		fmt.Printf("got: %s\n", got)
+		assert.Equal(t, tc.expected, got)
+	}
+}
