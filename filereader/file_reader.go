fd8644c796ac49855c1631a7f59c9783936dc2a4
latavin243 <arvin.qguo@outlook.com>
2023-12-06 14:35:58 +0800

    mod readfile

--- filereader/file_reader.go
+++ filereader/file_reader.go
@@ -6,8 +6,9 @@ import (
 )
 
 func ReadFile(filePath string, target interface{}) (err error) {
-	ft, isValid := getFileType(filePath)
-	if !isValid {
+	ext := filepath.Ext(filePath)
+	ft, supported := fileExtTypeMap[ext]
+	if !supported {
 		return fmt.Errorf("unsupported file type, filePath=%s", filePath)
 	}
 
@@ -16,13 +17,4 @@ func ReadFile(filePath string, target interface{}) (err error) {
 		return fmt.Errorf("file parser not found, filePath=%s", filePath)
 	}
 	return parser(filePath, target)
-}
-
-func getFileType(filePath string) (ft fileType, isValid bool) {
-	ext := filepath.Ext(filePath)
-	ft, ok := fileExtTypeMap[ext]
-	if !ok {
-		return fileTypeInvalid, false
-	}
-	return ft, true
 }
