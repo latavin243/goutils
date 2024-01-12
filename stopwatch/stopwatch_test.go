7e2b91f03f07676ca4c9f37506d29cbc2169dd07
latavin243 <arvin.qguo@outlook.com>
2024-01-12 10:55:15 +0800

    mod update stopwatch unittest

--- stopwatch/stopwatch_test.go
+++ stopwatch/stopwatch_test.go
@@ -5,20 +5,35 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/latavin243/goutils/stopwatch"
 )
 
 func TestStopwatch(t *testing.T) {
+	var elapsed float64
 	sw := stopwatch.New()
+
 	time.Sleep(2 * time.Second)
-	fmt.Printf("1st record, %f s\n", sw.Record().Seconds())
+	elapsed = sw.Record().Seconds()
+	fmt.Printf("1st record, %f s\n", elapsed)
+	assert.Equal(t, 2, int(elapsed))
+
 	time.Sleep(1 * time.Second)
-	fmt.Printf("2nd record, %f s\n", sw.Record().Seconds())
+	elapsed = sw.Record().Seconds()
+	fmt.Printf("2nd record, %f s\n", elapsed)
+	assert.Equal(t, 3, int(elapsed))
 
 	fmt.Println("[reset]")
 	sw.Reset()
+
 	time.Sleep(1 * time.Second)
-	fmt.Printf("1st record after reset, %f s\n", sw.Record().Seconds())
+	elapsed = sw.Record().Seconds()
+	fmt.Printf("1st record after reset, %f s\n", elapsed)
+	assert.Equal(t, 1, int(elapsed))
+
 	time.Sleep(2 * time.Second)
-	fmt.Printf("2nd record after reset, %f s\n", sw.Record().Seconds())
+	elapsed = sw.Record().Seconds()
+	fmt.Printf("2nd record after reset, %f s\n", elapsed)
+	assert.Equal(t, 3, int(elapsed))
 }
