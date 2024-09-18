e2a7a948e80266ea856418247f4d9873e2496283
latavin243 <arvin.qguo@outlook.com>
2024-09-18 15:31:23 +0800

    mod respcode enum

--- respcode/code_enum.go
+++ respcode/code_enum.go
@@ -6,21 +6,11 @@ var (
 	// below are some codes as example
 
 	// internal code
-	CodeInternalErr = RespCode{10000, "Internal error"} // common/unclassified internal error
+	CodeInternalErr = RespCode{10000, "Internal error"}
 
 	// input parameters code
-	CodeParamErr      = RespCode{20000, "Parameter error"} // common/unclassified parameter error
-	CodeNoAccess      = RespCode{20001, "No access"}
-	CodeInvalidPaging = RespCode{20002, "Invalid paging"}
-	CodeRateLimited   = RespCode{20003, "Rate limited"}
+	CodeParamErr = RespCode{20000, "Parameter error"}
 
 	// business code
-	CodeBusinessErr = RespCode{30000, "Business error"} // common/unclassified business error
-	CodeNoData      = RespCode{30001, "No data"}
-
-	// external service code
-	CodeExternalErr = RespCode{40000, "External service error"} // common/unclassified external service error
-
-	// external DB code
-	CodeDBQueryErr = RespCode{50001, "DB query error"}
+	CodeBusinessErr = RespCode{30000, "Business error"}
 )
